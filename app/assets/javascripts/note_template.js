1a20e2e97e6fbf091914dcc8b600291ce74e4ae1
boondawg <dan.jones@netspeed.com.au>
2014-04-03 17:05:27 +1100

    asd

--- app/assets/javascripts/note_template.js
+++ app/assets/javascripts/note_template.js
@@ -2,7 +2,31 @@ jQuery(document).ready(function() {
     $( "#sortable" ).sortable({
         placeholder: "ui-state-highlight",
 
-    update: function(){$('overvalue').val($("#sortable").sortable('serialize'))} });
+    //update: function(){$('overvalue').val($("#sortable").sortable('serialize'))} });
+    //update: function () { dotheupdateone($("#sortable").sortable('serialize', { key: "order" }))}});
+        update: function () {
+            var inputs = $('input.currentposition');
+            var nbElems = inputs.length;
+            $('input.currentposition').each(function(idx) {
+                $(this).val(nbElems - idx);
+            });
+        }
+    });
 
 })
 
+function dotheupdateone(order)
+{
+    alert(order);
+    $('#orderString').val(order);
+
+    function () {
+        var inputs = $('input.currentposition');
+        var nbElems = inputs.length;
+        $('input.currentposition').each(function(idx) {
+            $(this).val(nbElems - idx);
+        });
+    }
+});
+
+}
