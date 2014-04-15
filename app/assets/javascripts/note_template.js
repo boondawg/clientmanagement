d3c2c7ac1dd6860c6d04f5866658bc1145ef8acb
boondawg <dan.jones@netspeed.com.au>
2014-04-15 17:08:47 +1000

    added notes

--- app/assets/javascripts/note_template.js
+++ app/assets/javascripts/note_template.js
@@ -1,26 +1,42 @@
 jQuery(document).ready(function() {
     $( "#sortable" ).sortable({
         placeholder: "ui-state-highlight",
+        helper: function(e, tr)
+        {
+            var $originals = tr.children();
+            var $helper = tr.clone();
+            $helper.children().each(function(index)
+            {
+                // Set helper cell sizes to match the original sizes
+                $(this).width($originals.eq(index).width());
+            });
+            return $helper;
+        },
 
         //update: function(){$('overvalue').val($("#sortable").sortable('serialize'))} });
         //update: function () { dotheupdateone($("#sortable").sortable('serialize', { key: "order" }))}});
         update: function () {
-            var inputs = $('input.currentposition');
-            var nbElems = inputs.length;
-            var count = 1
-            $('input.currentposition').each(function(idx) {
-                $(this).val(count);
-                count ++
-            });
+           updateOrder();
         }
+    });
+    $('#sortable').on('cocoon:after-insert', function() {
+        updateOrder();
+    });
+    $('#sortable').on('cocoon:after-remove', function() {
+        updateOrder();
     });
 
 })
 
-function dotheupdateone(order)
+function updateOrder()
 {
-    alert(order);
-    $('#orderString').val(order);
+    var inputs = $('input.currentposition');
+    var nbElems = inputs.length;
+    var count = 1
+    $('input.currentposition').each(function(idx) {
+        $(this).val(count);
+        count ++
+    });
 
 }
 
